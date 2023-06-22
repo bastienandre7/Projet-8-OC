95701b507da4dac0d2f191beb0a390f8d700c986
Bastien Andre <bastienandre68@gmail.com>
2023-06-22 10:10:02 +0200

    utilisation du state sur le carrousel

--- src/components/Carrousel/carrousel.js
+++ src/components/Carrousel/carrousel.js
@@ -6,24 +6,16 @@ const Carrousel = ({length, source, title}) => {
     const [index, setIndex] = useState(0);
 
     function suivante(){
-        const img = document.getElementById("image");
-        const cpt = document.getElementById("compteur");
         const newIndex = index + 1;
         setIndex(newIndex >= length ? 0 : newIndex);
-        img.setAttribute("src", source[index]);
-        cpt.innerHTML = (index+1)+"/"+(length);
         return
     };
 
     
 
     function precedente(){
-        const img = document.getElementById("image");
-        const cpt = document.getElementById("compteur");
         const newIndex = index - 1;
         setIndex(newIndex < 0 ? length-1 : newIndex);
-        img.setAttribute("src", source[index]);
-        cpt.innerHTML = (index+1)+"/"+(length);
         return
     };
 
@@ -31,11 +23,11 @@ const Carrousel = ({length, source, title}) => {
     return(
 
         <div className='container-carousel'>
-                    <img src={source[0]} id='image' className='img-banner' alt={title} />
+                    <img src={source[index]} id='image' className='img-banner' alt={title} />
                     <i onClick={() => precedente()} id='arrow-carousel' className={length === 1 ? "none" : "fa-sharp fa-solid fa-chevron-left fa-5x arrow-left"}></i>
                     <i onClick={() => suivante()} id='arrow-carousel' className={length === 1 ? "none" : "fa-sharp fa-solid fa-chevron-right fa-5x arrow-right"}></i>
                     <div className="position">
-                        <p id='compteur' className={length === 1 ? "none" : "compteur"}>1/{length}</p>
+                        <p id='compteur' className={length === 1 ? "none" : "compteur"}>{(index+1)+"/"+(length)}</p>
                     </div>
                 </div>
 
